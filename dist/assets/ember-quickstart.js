1e46fee4718a7631e5796f51ddc8309880cbcd57
Leigh <leighbryant@bellsouth.net>
2017-01-18 16:37:37 -0500

    production attempt

--- dist/assets/ember-quickstart.js
+++ dist/assets/ember-quickstart.js
@@ -1183,7 +1183,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("ember-quickstart/app")["default"].create({"name":"ember-quickstart","version":"0.0.0+1b422983"});
+  require("ember-quickstart/app")["default"].create({"name":"ember-quickstart","version":"0.0.0+c1ab1eb6"});
 }
 
 /* jshint ignore:end */
