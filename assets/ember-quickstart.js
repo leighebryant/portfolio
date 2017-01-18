14b8830d30f01e59bab2a56b0a1b2210a75aee3f
Leigh <leighbryant@bellsouth.net>
2017-01-18 14:55:50 -0500

    troubleshooting

--- assets/ember-quickstart.js
+++ assets/ember-quickstart.js
@@ -1183,7 +1183,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("ember-quickstart/app")["default"].create({"name":"ember-quickstart","version":"0.0.0+68fb8b21"});
+  require("ember-quickstart/app")["default"].create({"name":"ember-quickstart","version":"0.0.0+0a61cca2"});
 }
 
 /* jshint ignore:end */
