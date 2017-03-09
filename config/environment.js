c5048604717a08db2dd3a3178eca9dd33c458c37
Leigh <leighbryant@bellsouth.net>
2017-03-08 21:49:06 -0500

    updating with new info

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-quickstart',
     environment: environment,
-    rootURL: '/portfolio/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
